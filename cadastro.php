e3709f1654369edb95232e89ea7afd6be32dbf0e
jorgefernandoaquino06@gmail.com <jorge.fernando@cs.up.edu.br>
2025-04-01 09:49:35 -0300

    index.php e cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Início - Atividade avaliativa 1</title>
+</head>
+<body>
+
+<h1>Calculadora de consumo de energia</h1>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')   {
+        $dados = $_POST;
+        if (!empty($dados['nome']) && !empty($dados['consumo']) && !empty($dados['uso_diario']) && !empty($dados['uso_mensal']) && !empty($dados['valor_kwh'])) {
+
+            $consumo_diario = ($dados['consumo'] / 1000) * $dados['uso_diario'];
+            $consumo_mensal = $consumo_diario * $dados['uso_mensal'];
+            $custo_mensal = $consumo_mensal * $dados['valor_kwh'];
+
+            echo "<h3>" . $dados['nome'] . "</h3>";
+            echo "<ul>";
+            echo "<li>Consumo diário: " . $consumo_diario . "  kWh</li>";
+            echo "<li>Consumo mensal: " . $consumo_mensal . " kWh</li>";
+            echo "<li>Custo mensal: " . $custo_mensal . " reais</li>";
+            echo "</ul>";
+
+            if($custo_mensal <= 5)    {
+                echo "<h3>Categoria de consumo: Baixa</h3>";
+            } else if (($custo_mensal > 5) && ($custo_mensal <= 10))   {
+                echo "<h3>Categoria de consumo: Média</h3>";
+            } else if ($custo_mensal > 10)   {
+                echo "<h3>Categoria de consumo: Elevada</h3>";
+            }
+
+        } else  {
+            echo "<h3>Preencha todos os campos.</h3>";
+        }
+
+    } else  {
+        echo "<h3>Nenhum dado de cliente enviado.</h3>";
+    }
+    ?>
+
+    <a href="index.php">Voltar ao início</a>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,9 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Início - Atividade avaliativa 1</title>
 </head>
 <body>
+
+<h1>Calculadora de consumo de energia</h1>
+<h3>Preencha o formulário abaixo para calcular o consumo de energia do seu aparelho.</h3>
+
+<form action="cadastro.php" method="post">
+    <p>
+    <label for="nome">Nome do aparelho:</label>
+    <input type="text" name="nome" required>
+    </p>
+
+    <p>
+    <label for="consumo">Consumo máximo (W):</label>
+    <input type="number" name="consumo" min=0 required>
+    </p>
+
+    <p>
+    <label for="uso_diario">Horas de uso no dia:</label>
+    <input type="number" name="uso_diario" min=0 required>
+    </p>
+
+    <p>
+    <label for="uso_mensal">Dias de uso no mês:</label>
+    <input type="number" name="uso_mensal" min=0 required>
+    </p>
+
+    <p>
+    <label for="valor_kwh">Valor do kWh (R$):</label>
+    <input type="number" name="valor_kwh" min=0 step=0.01 required>
+    </p>
+
+    <input type="submit" value="Calcular consumo">
+</form>
     
 </body>
 </html>
